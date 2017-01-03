dd33b8a0caf17397085fd894e8569690d9bca6fb
shozibabbas <14bscssabbas@seecs.edu.pk>
2017-01-03 20:52:43 +0500

    Update LSR.py

--- LSR.py
+++ LSR.py
@@ -6,16 +6,21 @@ from threading import Thread
 from time import sleep
 from socket import *
 import djikstra
+import os
+import subprocess as sp
 
 allRoutersDict = {}
 ackDict = {}
+activeRouters = {}
+deletedKeys = []
+djikstraTable = []
 
 def client(*args):
     routerId = args[0]
     allroutersPath = args[1]
     allroutersPort = args[2]
     
-    routersInfo = [routerId, allRoutersDict]
+    routersInfo = [routerId, allRoutersDict, ackDict]
         
     clientSocket = socket(AF_INET,SOCK_DGRAM)
     while(1):
@@ -40,32 +45,80 @@ def server(*args):
 	    
 	iRouterId = incomingRouterInfo[0]
 	iRouterDict = incomingRouterInfo[1]
-		
+	iRouterAckDict = incomingRouterInfo[2]
+	
+	for router in iRouterAckDict:
+	    if(activeRouters.has_key(router)):
+		activeRouters[router] = iRouterAckDict[router]
+	    else:
+		activeRouters.setdefault(router, iRouterAckDict[router])
+	for router in ackDict:
+	    if(activeRouters.has_key(router)):
+		activeRouters[router] = ackDict[router]
+	    else:
+		activeRouters.setdefault(router, ackDict[router])		
+
+	for key in ackDict:
+	    ackDict[key] = ackDict[key] + 1
+	
+	ackDict[iRouterId] = 0
+	"""
+	for delKey in ackDict:
+	    if(delKey > 3):
+		for key in allRoutersDict:
+		    if(key[0] == delKey or key[1] == delKey):
+			deletedKeys.append(key)
+	for key in deletedKeys:
+	    if allRoutersDict.has_key(key):
+		del allRoutersDict[key]"""
+			
 	for router in iRouterDict:
 	    if not (allRoutersDict.has_key((router[0], router[1])) or allRoutersDict.has_key((router[1], router[0]))):
 		allRoutersDict.setdefault(router, iRouterDict[router])
-	sleep(1)	
+	sleep(0.1)
+    return true
 	    
-def djikstraShortestPath():
+def djikstraShortestPath(*args):
     while(1):
 	sleep(30)
+	os.system('cls')
 	edgeNodes = []
+	djikstraTable = []
+	allNodes = set()
 	for key in allRoutersDict:
+	    if key[0] in activeRouters and activeRouters[key[0]] > 3 or key[1] in activeRouters and activeRouters[key[1]] > 3:
+		continue
 	    fromNode = ""
 	    toNode = ""
 	    path = 0.0
 	    fromNode = key[0]
 	    toNode = key[1]
 	    path = allRoutersDict[key]
-	    
+	    allNodes.add(fromNode)
+	    allNodes.add(toNode)
 	    s = (fromNode, toNode, path)
 	    edgeNodes.append(s)
-	    	
-	print djikstra.dijkstra(edgeNodes, "A", "E")
+	for n in allNodes: 
+	    if not (n == args[0]):
+		djikstraTable.append((args[0], n, djikstra.dijkstra(edgeNodes, args[0], n)))
 	
+	print "I am Router ", args[0]
 	
+	for row in djikstraTable:
+	    if row[1] == float("inf") or row[1] in activeRouters and activeRouters[row[1]] <= 3:
+		print "Least cost path to router ", row[1],":", convertToPath(row[2][1]), " and the cost: ", row[2][0]
     return True
 
+def convertToPath(dPath):
+    if(dPath[1] == ()):
+	if not dPath[0] == None:
+	    print (dPath[0]),
+    else:   
+	convertToPath(dPath[1])
+	if not dPath[0] == None:
+	    print (dPath[0]),
+    return
+    
 
 if __name__ == "__main__":
    
@@ -92,12 +145,10 @@ if __name__ == "__main__":
 	routersPort.setdefault(x[0], int(x[2]))
 	allRoutersDict.setdefault((routerId, x[0]), float(x[1]))
 	ackDict.setdefault(x[0], 0)
-
-    print "server: current dict = ", allRoutersDict
     
     thread1 = Thread(target = server, args = [routerPortNumber, routersPort, routerId])
     thread2 = Thread(target = client, args = [routerId, routersPath, routersPort])
-    thread3 = Thread(target = djikstraShortestPath)
+    thread3 = Thread(target = djikstraShortestPath, args = [routerId])
     thread1.start()
     thread2.start()
     thread3.start()
